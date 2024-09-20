540cb06bfe397814e22ba2aa676df87947bc1dda
Pintu <pintu@Pintus-Laptop.lan>
2024-09-20 20:28:38 +0530

    update experience

--- src/Data/IntershipData.js
+++ src/Data/IntershipData.js
@@ -1,4 +1,13 @@
 const internship =[
+  {
+    "image": "https://i.postimg.cc/K8QFJGFw/1705389198122.jpg",
+    "title": "Software Developer",
+    "company":"Soluperts.ca",
+    "duration":"Jan. 2024 - July 2024",
+    "location":"Remote, Canada",
+    "body":"Implementing the solution using MongoDB, Express.js, React.js,JavaScript and Node.js to create a scalable web system. This includes designing role-based access control, data consolidation, reporting, and user interfaces.Implementing the solution using MongoDB, Express.js, React.js,JavaScript and Node.js to create a scalable web system. This includes designing role-based access control, data consolidation, reporting, and user interfaces.",
+    "user_id":1
+  },
 
     {
         "image": "/Images/mantra.jpeg",
@@ -7,7 +16,7 @@ const internship =[
         "duration":"Aug. 2023 - Oct. 2023",
         "location":"New Delhi,India",
         "body":"I developed the front-end of an ICU digitization website using React and Bootstrap, connected it to the back-end with JavaScript, and resolved Cors errors in the ISAPI Protocol, improving website functionality",
-        "user_id":1
+        "user_id":2
       },
 
       {
@@ -18,7 +27,7 @@ const internship =[
         "location":"New Delhi,India",
         "body":"E-commerce, powered by mobile technology, reshapes global shopping habits. 'thecart,' an Android app, simplifies online shopping with product sections, banners, tracking, and Firebase data management, fueling the rapid growth of the E-commerce market.",
         // "projectlink":"https://github.com/rajandu/thecart2",
-        "user_id":2
+        "user_id":3
 
     }
       
--- src/Data/WorkData.js
+++ src/Data/WorkData.js
@@ -1,13 +1,14 @@
 const work=[
     // sample data format
-    // {
-    //     "image": "/Images/mantra.jpeg",
-    //     "title": "Software Developer",
-    //     "company":"Holy Basil Matra Pvt. Ltd.",
-    //     "duration":"Aug. 2023 - Oct. 2023",
-    //     "location":"New Delhi India",
-    //     "body":"I developed the front-end of an ICU digitization website using React and Bootstrap, connected it to the back-end with JavaScript, and resolved Cors errors in the ISAPI Protocol, improving website functionality",
-    //     "user_id":1
-    //   },
+    {
+    "image": "https://i.postimg.cc/K8QFJGFw/1705389198122.jpg",
+    "title": "Software Developer",
+    "company":"Soluperts.ca",
+    "duration":"Aug. 2024 - Present",
+    "location":"Remote, Canada",
+    "body":"Implementing the solution using MongoDB, Express.js, React.js,JavaScript and Node.js to create a scalable web system. This includes designing role-based access control, data consolidation, reporting, and user interfaces.Implementing the solution using MongoDB, Express.js, React.js,JavaScript and Node.js to create a scalable web system. This includes designing role-based access control, data consolidation, reporting, and user interfaces.",
+    "user_id":1
+  },
+
 ]
 export default work;
