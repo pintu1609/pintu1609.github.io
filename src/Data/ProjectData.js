663ec0a1d3eb9956f53085fbc4b70601b47e052d
Pintu <pintu@Mac.lan>
2025-04-16 22:48:24 +0530

    add one more project chatting app

--- src/Data/ProjectData.js
+++ src/Data/ProjectData.js
@@ -1,4 +1,19 @@
 const projectdata=[
+  {
+    "image":[{"img":<i className="fa-brands fa-html5 fa-2xl mx-2 my-4" style={{color: '#e83211'}}></i>,
+    "subtitle":"HTML"},
+    {"img": <i className="fa-brands fa-css3 fa-2xl mx-2 my-4" style={{color: '#3666ba'}}></i>,
+    "subtitle":"CSS"
+    },{"img":<i className="fa-brands fa-js fa-2xl mx-2 my-4" style={{color: '#df5f34'}}></i>,
+  "subtitle":"JavaScript"},{"img":<i className="fa-brands fa-react fa-2xl mx-2 my-4" style={{color: '#3cb3c3'}}></i>,
+"subtitle":"React"}],
+    "title":"Chatting Web Application",
+    "duration":"Dec. 2023 - Jan. 2024",
+    "body":"Create a dynamic real-time chatting app with React, Node.js, MongoDB, and Socket.io. Utilize React for the frontend, Node.js for the server, MongoDB for data storage, and Socket.io for seamless, instant communication between users.Create a dynamic real-time chatting app with React, Node.js, MongoDB, and Socket.io. Utilize React for the frontend, Node.js for the server, MongoDB for data storage, and Socket.io for seamless, instant communication between users.",
+    "projectlink":"https://chatting-app-frontend-five.vercel.app/",
+    "user_id":1
+
+  },
     {
         "image":[ {"img":<i className="fa-brands fa-html5 fa-2xl mx-2 my-4" style={{color: '#e83211'}}></i>,
                     "subtitle":"HTML"},
@@ -12,8 +27,9 @@ const projectdata=[
         "duration":"Feb. 2023 - April 2023",
         "body":"Hospital Management System (HMS) streamlines appointments, enabling remote booking, doctor availability checks, and online payments. It boosts administrative efficiency, benefiting patients and healthcare providers alike. An indispensable tool for diverse healthcare facilities, HMS enhances the overall healthcare experience and operational effectiveness.",
         "projectlink":"https://github.com/pintu1609/hospital-management-system",
-        "user_id":1
+        "user_id":2
       },
+     
       {
         "image":[{"img":<i className="fa-brands fa-html5 fa-2xl mx-2 my-4" style={{color: '#e83211'}}></i>,
         "subtitle":"HTML"},
@@ -25,8 +41,8 @@ const projectdata=[
         "title":"iNotebook",
         "duration":"June 2023 - June 2023",
         "body":"iNotebook revolutionizes note-taking. Seamlessly create, edit, and manage notes for enhanced organization. This digital notepad streamlines your tasks, from jotting ideas to setting tasks. Say goodbye to clutter and hello to productivity with iNotebook.",
-        "projectlink":"https://github.com/pintu1609/iNotebook",
-        "user_id":2
+        "projectlink":"https://pintu-inotebook.vercel.app/login",
+        "user_id":3
 
       },
      
@@ -39,7 +55,7 @@ const projectdata=[
             "duration":"July 2023 - July 2023",
             "body":"NewsMonkey is your go-to news app for quick, bite-sized daily updates. Whether you're passionate about current events, weather forecasts, politics, or sports news, NewsMonkey has you covered. Stay informed and up-to-date with a wide range of topics, all conveniently accessible in one app. Get your daily news fix with NewsMonkey and stay in the loop effortlessly.",
             "projectlink":"https://github.com/pintu1609/newsapp",
-            "user_id":3
+            "user_id":4
 
         },
         {
@@ -49,7 +65,7 @@ const projectdata=[
               "duration":"Aug. 2022 - Nov. 2022 ",
               "body":"The paper uses Recursive Neural Networks to build two classifiers: one to identify Technology Determinism or Skepticism and another for Pro or Anti Policy sentiment in media. It employs natural language processing to detect biases in media and predict sentiment, delving into complex language nuances.",
               "projectlink":"https://jmscm.smartsociety.org/volume4_issue3/paper8.pdf",
-              "user_id":4
+              "user_id":5
   
           },
           {
@@ -59,7 +75,7 @@ const projectdata=[
                 "duration":"Jan. 2023 - April 2023",
                 "body":"This study investigates the interconnected dynamics of hazards, their vulnerability, and their implications on the environment, resulting in an elevated risk of fatalities. It seeks to understand the complex relationship between these factors and their consequences on human and ecological well-being.",
                 "projectlink":"https://drive.google.com/file/d/1LTsznwAaFXWds1gunp7is2wvOV-FFmHR/view?usp=drive_link",
-                "user_id":5
+                "user_id":6
     
             },
             {
@@ -69,7 +85,7 @@ const projectdata=[
                   "duration":"Feb. 2022 - April 2022",
                   "body":"The Grader Android learning app enables users to take mock tests in various subjects, each containing different questions, all within a set time limit. Post-test, users can assess their scores, view leaderboard rankings, and bookmark questions for reference. Additionally, they can review correct answers to all questions, enhancing their learning experience. ",
                   "projectlink":"https://github.com/pintu1609/Learning-app-Grader",
-                  "user_id":6
+                  "user_id":7
       
               }
               
--- src/Data/WorkData.js
+++ src/Data/WorkData.js
@@ -2,9 +2,9 @@ const work=[
     // sample data format
     {
     "image": "https://i.postimg.cc/K8QFJGFw/1705389198122.jpg",
-    "title": "Software Developer",
+    "title": "Full Stack Developer",
     "company":"Soluperts.ca",
-    "duration":"Aug. 2024 - Present",
+    "duration":"Aug. 2024 - Feb. 2025",
     "location":"Remote, Canada",
     "body":"Implementing the solution using MongoDB, Express.js, React.js,JavaScript and Node.js to create a scalable web system. This includes designing role-based access control, data consolidation, reporting, and user interfaces.Implementing the solution using MongoDB, Express.js, React.js,JavaScript and Node.js to create a scalable web system. This includes designing role-based access control, data consolidation, reporting, and user interfaces.",
     "user_id":1
